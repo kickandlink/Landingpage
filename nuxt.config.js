e0ccad89acd2ca4219ea54e83654381403738a35
adilmania <adil.mania.pro@gmail.com>
2019-03-09 22:38:08 +0100

    :tada: Upload landing V1.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,30 +1,61 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'landingpage',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    title: 'Kick & Link - Le Match Parfait',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070'
+  },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
+  css: [
+    // SCSS file in the project
+    '@/assets/css/main.scss'
+  ],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Simple usage
+    'nuxt-buefy',
+  ],
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+     ** Run ESLint on save
+     */
+    extend(config, {
+      isDev,
+      isClient
+    }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
@@ -36,4 +67,3 @@ module.exports = {
     }
   }
 }
-
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,44 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">Kick & Link</h1>
-      <h2 class="subtitle">Landing Page (by Adil Mania)</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+  <section class="hero is-white is-fullheight is-paddingless">
+    <div class="hero-head is-paddingless">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="../">
+              <img src="@/assets/img/Logo.png" alt="Logo">
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <div class="hero-body is-paddingless">
+      <div class="container has-text-centered is-multiline">
+        <div class="column">
+          <h1 class="title is-size-3">Le Match Parfait</h1>
+          <h2
+            class="subtitle is-size-6"
+          >Avec Kick & Link, Nous offrons à nos joueurs de rugby semi-pro plus de visibilité face aux meilleurs clubs, grâce à leurs CVs sportifs et professionnels, et pour les clubs, un large choix de joueurs est garanti.</h2>
+          <img style="width: 500px;" src="@/assets/img/draw.png" alt="Kick and Link">
+        </div>
+        <div class="columns">
+          <div class="column grow">
+            <h1 class="title is-spaced">Plus de visibilité, CV sportif & pro</h1>
+            <h2 class="subtitle">L'offre parfaite pour toi</h2>
+            <a
+              href="https://kickandlink.typeform.com/to/nP1jdL"
+              class="button is-primary is-medium"
+            >Pour les Joueurs</a>
+          </div>
+          <div class="column grow">
+            <h1 class="title is-size-3 is-spaced">Un large choix de joueurs</h1>
+            <h2 class="subtitle">Un meilleur recrutement</h2>
+            <a
+              href="https://kickandlink.typeform.com/to/X6j5wE"
+              class="button is-black is-medium"
+            >Pour les Clubs</a>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -23,35 +55,5 @@ export default {
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css?family=Dosis");
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Dosis", sans-serif;
-  /* font-family: """Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
 
