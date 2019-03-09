2551c9b01325554a95baf8af998d813159cffbdf
Adil Mania <adil.mania.pro@gmail.com>
2019-03-09 16:48:25 +0100

    :tada: Test 2

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <app-logo/>
       <h1 class="title">Kick & Link</h1>
-      <h2 class="subtitle">Landing Page</h2>
+      <h2 class="subtitle">Landing Page (Made by Adil Mania)</h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
