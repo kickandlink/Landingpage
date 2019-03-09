80d98bfc402ec922e76dc06d1ef1ed6310cd3196
Adil Mania <adil.mania.pro@gmail.com>
2019-03-09 16:38:54 +0100

    :tada: Test modif

--- pages/index.vue
+++ pages/index.vue
@@ -2,37 +2,28 @@
   <section class="container">
     <div>
       <app-logo/>
-      <h1 class="title">
-        landingpage
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
+      <h1 class="title">Kick & Link</h1>
+      <h2 class="subtitle">Landing Page</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue";
 
 export default {
   components: {
     AppLogo
   }
-}
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Dosis");
 .container {
   min-height: 100vh;
   display: flex;
@@ -42,7 +33,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Dosis", sans-serif;
+  /* font-family: """Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; */
   display: block;
   font-weight: 300;
   font-size: 100px;
