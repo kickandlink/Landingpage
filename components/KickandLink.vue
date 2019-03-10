bd6a76a5a2090fe973a07cbb902ac07678356996
adilmania <adil.mania.pro@gmail.com>
2019-03-10 16:09:24 +0100

    :tada: add KickandLink section

--- components/KickandLink.vue
+++ components/KickandLink.vue
@@ -1,5 +1,12 @@
 <template>
-  <section class="hero is-white is-paddingless"></section>
+  <section class="hero is-primary is-medium">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">Prêt pour le Kick & Link?</h1>
+        <h2 class="subtitle">Découvrez comment Kick & Link fonctionne pour vous</h2>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
